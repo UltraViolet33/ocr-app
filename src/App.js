6ef0bdcbf297e6cf2b86c5337ca142e3d1bbb1e9
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-03-29 11:36:01 +0200

    add Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "../css/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>OCR APP</h1>
+    </header>
+  );
+};
+
+export default Header;
